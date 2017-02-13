21b55e0c4cf92b08b46db02f415809411f35cbe4
doge <such-shibe@users.noreply.github.com>
2017-02-13 00:56:18 +0000

    ADDED the start of the persistence code

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const locator  = require.main.require("./lib/locator/locator");
-const castBool = val => 
-    Boolean(JSON.parse((val || false).toString().toLowerCase()));
-const liveMode = castBool(process.env.DEBUG);
-const testMode = castBool(process.env.RELEASE);
+const mode = process.env.NODE_ENV || "development";
+
+console.log("starting in '" + mode  + "' mode.");
 
 // provide a shorthand for getting inject modules for this context
 global.provide = name => require.main.require(locator.get(name));
 
-if(liveMode) {
-    console.log("Live Mode\n");
-
-    locator.register("config", "./lib/config/config-release");
-    locator.register("cache", "./lib/cache/memory"); // TODO replace with redis
-} else if(testMode) {
-    console.log("Test Mode\n");
-    
-    locator.register("config", "./lib/config/config-test");
-    locator.register("cache", "./lib/cache/memory"); // TODO replace with redis
-} else {
-    console.log("Dev Mode\n");
-    
-    locator.register("config", "./lib/config/config-debug");
-    locator.register("cache", "./lib/cache/memory");
+switch(mode) {
+    case: "live" :
+          locator.register("config", "./lib/config/config-release");
+          locator.register("cache", "./lib/cache/none"); // TODO replace with redis
+          break;
+    case "test" :
+          locator.register("config", "./lib/config/config-test");
+          locator.register("cache", "./lib/cache/memory"); // TODO replace with redis
+          break;
+    case "development":
+    default:
+          locator.register("config", "./lib/config/config-debug");
+          locator.register("cache", "./lib/cache/memory");
+          break;
 }
 
 require("./lib/server");
